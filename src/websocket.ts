aa73b8ec7f3c5bf680978d92610607d95b93f488
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-10-09 11:46:18 -0300

    Returning data in the socket

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,27 @@
 import { io } from './http';
+import UUIDGenerator from './utils/uuidGenerator';
+
+interface IMessage {
+  socket_id: string;
+  message: string;
+  sentAt: string;
+  id: string;
+}
+
+const messages: Array<IMessage> = [];
 
 io.on('connection', (socket) => {
-  console.log(socket.id)
+  socket.on('send-message', (message: string, callback: (messages: Array<IMessage>) => void) => {
+    const messageObj: IMessage = {
+      message,
+      sentAt: new Date().toISOString(),
+      socket_id: socket.id,
+      id: new UUIDGenerator().generate()
+    }
+
+    messages.push(messageObj);
+    callback(messages)
+  })
+
+  socket.emit('messages', messages)
 })
